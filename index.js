d247b44e610ee78ac8a5c954e90d13997b12bd73
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-27 04:03:33 -0400

    agregando funcion leer archivo si es archivo md

--- index.js
+++ index.js
@@ -8,19 +8,26 @@ const path= require('path')
 // const mdLinks = require ('./md-links')
 
 
+
 let pathUser= process.argv[2];//RUTA DEL ARCHIVO MD
 
 let pathFile = path.resolve(pathUser)
-console.log('ruta del usuario:',pathFile)
+// console.log('ruta del usuario:',pathFile)
 
-function directory(pathFile) {
-fs.lstat(pathFile, (err, stats) => {
+function directory(path) {
+fs.lstat(path, (err, stats) => {
       if(err)
-          return console.log(err)
-      console.log(`Is file: ${stats.isFile()}`);
-      console.log(`Is directory: ${stats.isDirectory()}`);
+          return console.log(err);
+    //   console.log(`Is file: ${stats.isFile()}`);
+    //   console.log(`Is directory: ${stats.isDirectory()}`);
+      if(stats.isFile()===true){
+        links(pathUser)
+      console.log('es file') }
+      if (stats.isDirectory()===true){
+     console.log('es directorio')}
   });
-}console.log(directory())
+};
+directory(pathUser)
 
 
 
@@ -31,26 +38,39 @@ fs.lstat(pathFile, (err, stats) => {
 
 //-------CREAMOS UNA CONSTANTE PARA QUE LEA UN ARCHIVO CON UN PATH (RUTA) ESPECIFICA , USAMOS MARKED Y FS---
 
-// const links = (path1)=>{
-//   fs.readFile(path1,'utf8', (err, data)=>{
-// if (err){
-//   console.log('error al igresar ruta')
-//   throw err;
-// } 
-// let links = [];
-// const renderer = new marked.Renderer();
-// renderer.link = function (href, title, text){
-//   links.push({
-//     href:href,
-//     text:text,
-//     file:path1
-//   })
-// }
-// marked(data,{renderer:renderer});
-// console.log(links)
-//   })
-// }
-// console.log(links(path1))
+function links (pathUser){
+  fs.readFile(pathUser,'utf8', (err, data)=>{
+if (err){
+
+  throw err;
+} 
+let links = [];
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text){
+  links.push({
+    href:href,
+    text:text,
+    file:pathUser
+  })
+}
+marked(data,{renderer:renderer});
+console.log(links)
+  })
+}
+console.log(links(pathUser))
+
+//---------- CREAMOS UNA CONSTANTE PARA LEER ARCHIVOS MD DE UN DIRECTORIO---------------------
+
+function files(pathUser){
+  FileHound.create()
+  .paths(pathUser)
+  .ext('md')
+  .find()
+ .then(files =>{
+files.forEach(file =>console.log('Found file', file));
+})
+};
+console.log(files(pathUser))
 
 
 
--- md-links.js
+++ md-links.js
@@ -1,36 +1,29 @@
-// // lee los archivos md de un directorio
-// const files = (path) =>{
-//   FileHound.create()
-//   .paths(path)
-//   .ext('md')
-//   .find()
-//  .then(files =>{
-// files.forEach(file =>console.log('Found file', file));
-// })
-// };
-// console.log(files('../SCL009-md-links'))
+// lee los archivos md de un directorio
+const FileHound = require('filehound');
+let pathUser= process.argv[2];
+const path= require('path')
 
-//lee archivos por el momento no lo cocuparemos por ser poco especifico
-// const links = (path)=>{
-//   fs.readdir(path,'utf8', (err, data)=>{
-// if (err){
-//   throw err;
-// } 
-// console.log(data)
-//   })
-// }
-// console.log(links('../SCL009-md-links'))
+const files = (pathUser) =>{
+  FileHound.create()
+  .paths(pathUser)
+  .ext('md')
+  .find()
+ .then(files =>{
+files.forEach(file =>console.log('Found file', file));
+})
+};
+console.log(files(pathUser))
+// ../SCL009-md-links'
 
+// const fs = require("fs");
 
-const fs = require("fs");
+// let path = process.argv[2]
 
-let path = process.argv[2]
+// fs.lstat(path, (err, stats) => {
+// console.log('entrando')
+//     if(err)
+//         return console.log(err); //Handle error
 
-fs.lstat(path, (err, stats) => {
-console.log('entrando')
-    if(err)
-        return console.log(err); //Handle error
-
-    console.log(`Is file: ${stats.isFile()}`);
-    console.log(`Is directory: ${stats.isDirectory()}`);
-});
+//     console.log(`Is file: ${stats.isFile()}`);
+//     console.log(`Is directory: ${stats.isDirectory()}`);
+// });
