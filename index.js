7d961107987f4e0eaf2dc5f4f071ae5acc312096
luisaromero <luisa.marcela.romero@gmail.com>
2019-06-28 15:27:27 -0400

    dividiendo las funciones a archivo index y md links

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const marked = require('marked');
 const FileHound = require('filehound');
 const path= require('path')
-// const mdLinks = require ('./md-links')
+const mdLinks = require ('./md-links')
 
 //RUTA DEL ARCHIVO MD
 let pathUser= process.argv[2];
@@ -18,46 +18,21 @@ fs.lstat(path, (err, stats) => {
         console.log(err);
       } else if (stats.isDirectory()){
       console.log( 'soy directorio');
-      readUserDirectory(path);
+      mdLinks.readUserDirectory(path);
       }else {
      console.log('soy file')
-     readUserFiles(pathUser)
+     mdLinks.readUserFiles(pathUser)
       }
   });
 };
 
 //-------CREAMOS UNA CONSTANTE PARA QUE LEA UN ARCHIVO CON UN PATH (RUTA) ESPECIFICA , USAMOS MARKED Y FS---
 
-function readUserFiles (pathUser){
-  fs.readFile(pathUser,'utf8', (err, data)=>{
-if (err){
-throw err;
-} 
-let links = [];
-const renderer = new marked.Renderer();
-renderer.link = function (href, title, text){
-  links.push({
-    href:href,
-    text:text,
-    file:pathUser
-  })
-}
-marked(data,{renderer:renderer});
-console.log(links)
-  })
-}
+
 
 //---------- CREAMOS UNA CONSTANTE PARA LEER ARCHIVOS MD DE UN DIRECTORIO---------------------
 
-function readUserDirectory(path){
-  FileHound.create()
-  .paths(path)
-  .ext('md')
-  .find()
- .then(files =>{
-files.forEach(file => console.log(readUserFiles(file)));
-})
-};
+
 
 isFileOrDirectory(pathFile)
 
--- md-links.js
+++ md-links.js
@@ -1,24 +1,69 @@
-var fetchUrl = require("fetch").fetchUrl;
 const fs = require('fs');
+const marked = require('marked');
+const FileHound = require('filehound');
+const path= require('path')
 
-let url = "https://www.google.com";
-const getData = (url) => {
-  return new Promise ((resolve, reject)=> {
-    fetchUrl(url, (error, meta, body) =>{
-          if (meta){
-              resolve(meta.status);
-          }else{
-              reject (error);
-          }
-      })
-  })
+
+
+
+
+
+function readUserFiles (pathUser){
+  fs.readFile(pathUser,'utf8', (err, data)=>{
+if (err){
+throw err;
 } 
+let links = [];
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text){
+  links.push({
+    href:href,
+    text:text,
+    file:pathUser
+  })
+}
+marked(data,{renderer:renderer});
+console.log(links)
+  })
+}
 
 
-getData(url)
-.then(res => {
-  console.log("El estado del sitio", url, "es:",res)
+function readUserDirectory(path){
+  FileHound.create()
+  .paths(path)
+  .ext('md')
+  .find()
+ .then(files =>{
+files.forEach(file => console.log(readUserFiles(file)));
 })
-.catch(err => {
-  console.log(err);
-})
+};
+ module.exports.readUserFiles=readUserFiles
+ module.exports.readUserDirectory=readUserDirectory
+
+
+
+
+// var fetchUrl = require("fetch").fetchUrl;
+// const fs = require('fs');
+
+// let url = "https://www.google.com";
+// const getData = (url) => {
+//   return new Promise ((resolve, reject)=> {
+//     fetchUrl(url, (error, meta, body) =>{
+//           if (meta){
+//               resolve(meta.status);
+//           }else{
+//               reject (error);
+//           }
+//       })
+//   })
+// } 
+
+
+// getData(url)
+// .then(res => {
+//   console.log("El estado del sitio", url, "es:",res)
+// })
+// .catch(err => {
+//   console.log(err);
+// })
